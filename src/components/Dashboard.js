81131e3d6f94601e58a2904a551b97d2206337dc
Adenike247 <barakah24777@gmail.com>
2024-02-05 11:40:32 +0100

    setup next.js

---
+++ src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde explicabo, quia harum tempore voluptatibus, consequatur tenetur illum ipsam placeat, dignissimos similique optio iste officiis exercitationem nihil qui facere labore esse?
+    </div>
+  )
+}
+
+export default Dashboard
